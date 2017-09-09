e29f3e400baf28f51d8226e9c0cf4e6c11f025a8
Tharre <tharre3@gmail.com>
2017-09-09 18:04:47 +0200

    Test10

--- .build_pkg.sh
+++ .build_pkg.sh
@@ -3,15 +3,17 @@
 useradd -m -s /bin/bash builder
 chown builder:builder /build
 echo "builder ALL=(ALL) NOPASSWD: /usr/bin/pacman" >> /etc/sudoers
-
 echo 'Server = http://mirror.th73.ovh/$repo/os/$arch' > /etc/pacman.d/mirrorlist
-pacman -Syu
 
-sudo -u builder mkdir -p /home/builder/.gnupg
-cat << EOF > /home/builder/.gnupg/gpg.conf
+sudo -u builder -s -- <<EOF
+sudo pacman -Syu
+
+mkdir -p ~/.gnupg
+cat << END > ~/.gnupg/gpg.conf
 keyserver-options auto-key-retrieve
 keyserver hkp://ipv4.pool.sks-keyservers.net
-EOF
+END
 
 cd /build
-sudo -u builder makepkg -s --noconfirm
+makepkg -s --noconfirm
+EOF
